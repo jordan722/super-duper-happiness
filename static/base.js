de556c68d0cf6809a3cd4ae8660a9ddb1f88c4da
Sarah Yoon <syoon2@stuy.edu>
2017-03-30 02:00:26 -0400

    plot(year) works for slightly different json format

--- static/base.js
+++ static/base.js
@@ -3,32 +3,35 @@ var svg = d3.select("#svg");
 var w = 850;
 var h = 550;
 
-var dataset = [
+var dataset =
     {
-	"country": "Dummy",
-	"total": 0,
-	"apprehended": 1,
-	"naturalized": 1,
-	"highlight": false,
-	"region": ""
-    },
-    {
-	"country": "Canada",
-	"total": 500,
-	"apprehended": 0.1,
-	"naturalized": 0.9,
-	"highlight": false,
-	"region": "North America"
-    },
-    {
-	"country": "Egypt",
-	"total": 382,
-	"apprehended": 0.38,
-	"naturalized": 0.62,
-	"highlight": false,
-	"region": "North Africa"
-    }
-];
+	"2005": [
+	    {
+		"country": "Dummy",
+		"total": 0,
+		"apprehended": 1,
+		"naturalized": 1,
+		"highlight": false,
+		"region": ""
+	    },
+	    {
+		"country": "Canada",
+		"total": 500,
+		"apprehended": 0.1,
+		"naturalized": 0.9,
+		"highlight": false,
+		"region": "North America"
+	    },
+	    {
+		"country": "Egypt",
+		"total": 382,
+		"apprehended": 0.38,
+		"naturalized": 0.62,
+		"highlight": true,
+		"region": "North Africa"
+	    }
+	]
+    };
 
 var regionColors =
     {
@@ -52,40 +55,56 @@ var regionColors =
 //This can be reduced to continents though idrc
 
 var xScale = d3.scale.linear()
-    .domain([0, d3.max(dataset, function(d) { return d["naturalized"]; })])
+    .domain([0, 1])
     .range([0, w]);
 
 var yScale = d3.scale.linear()
-    .domain([0, d3.max(dataset, function(d) { return d["apprehended"]; })])
+    .domain([0, 1])
     .range([h, 0]);
 
-svg.selectAll("circle")
-    .data(dataset)
-    .enter()
-    .append("circle")
-    .attr("cx", function(d) {
-        return xScale(d["naturalized"]);
-    })
-    .attr("cy", function(d) {
-        return yScale(d["apprehended"]);
-    })
-    .attr("fill", function(d) {
-	return regionColors[d["region"]];
-    })
-    .attr("r", function(d) {
-	return d["total"]/10;
-    });
+var plot = function(year) {
+    svg.selectAll("circle")
+	.data(dataset[year.toString()])
+	.enter()
+	.append("circle")
+	.attr("cx", function(d) {
+            return xScale(d["naturalized"]);
+	})
+	.attr("cy", function(d) {
+            return yScale(d["apprehended"]);
+	})
+	.attr("fill", function(d) {
+	    return regionColors[d["region"]];
+	})
+	.attr("stroke", function(d) {
+	    return regionColors[d["region"]];
+	})
+	.attr("fill-opacity", function(d) {
+	    if (d["highlight"] == true) {
+		return 1;
+	    }
+	    else {
+		return 0.5;
+	    }
+	})		    
+	.attr("r", function(d) {
+	    return d["total"]/10;
+	});
 
-var formatAsPercentage = d3.format(".1%");
+    var formatAsPercentage = d3.format(".1%");
 
-var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
-var yAxis = d3.svg.axis().scale(yScale).orient("left");
+    var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+    var yAxis = d3.svg.axis().scale(yScale).orient("left");
 
-svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(0," + h + ")")
-    .call(xAxis.tickFormat(formatAsPercentage));
+    svg.append("g")
+	.attr("class", "axis")
+	.attr("transform", "translate(0," + h + ")")
+	.call(xAxis.tickFormat(formatAsPercentage));
 
-svg.append("g")
-    .attr("class", "axis")
-    .call(yAxis.tickFormat(formatAsPercentage));
+    svg.append("g")
+	.attr("class", "axis")
+	.call(yAxis.tickFormat(formatAsPercentage));
+
+};
+
+plot(2005);
